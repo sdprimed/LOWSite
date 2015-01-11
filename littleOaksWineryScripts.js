b4c14a434c881ef0de2707db352e43ea614741ce
siggyt <siggy.t@gmail.com>
2015-01-11 10:12:18 -0800

    separating the sections into separate html files for external linkages

--- littleOaksWineryScripts.js
+++ littleOaksWineryScripts.js
@@ -3,96 +3,7 @@
 $(document).ready(
 	function()
 	{
-			$(".menuHome").click(function()
-			{
-				$(".contentHome").show();
-				$(".contentHistory").hide();
-				$(".contentGallery").hide();
-				$(".contentProducts").hide();
-				$(".contentWineClub").hide();
-				$(".contentAbout").hide();
-				$(".contentContactUs").hide();
-				$(".sidePanelHomeLinks" ).show();
-				$(".sidePanelHistoryLinks" ).hide();
-				$(".sidePanelGalleryLinks" ).hide();
-			});
-								
-			$(".menuHistory").click(function()
-			{
-				$(".contentHome").hide();
-				$(".contentHistory").show();
-				$(".contentGallery").hide();
-				$(".contentProducts").hide();
-				$(".contentWineClub").hide();
-				$(".contentAbout").hide();
-				$(".contentContactUs").hide();
-				$(".sidePanelHomeLinks" ).hide();
-				$(".sidePanelHistoryLinks" ).show();
-				$(".sidePanelGalleryLinks" ).hide();
-				$(".historyOfLOW").show();
-				$(".historyOfSDWines").hide();
-			});
-								
-			$(".menuGallery").click(function()
-			{
-				$(".contentHome").hide();
-				$(".contentHistory").hide();
-				$(".contentGallery").show();
-				$(".contentProducts").hide();
-				$(".contentWineClub").hide();
-				$(".contentAbout").hide();
-				$(".contentContactUs").hide();
-				$(".sidePanelHomeLinks" ).hide();
-				$(".sidePanelHistoryLinks" ).hide();
-				$(".sidePanelGalleryLinks" ).show();
-				$(".galleryWhiteGrapes").show();
-				$(".galleryRedGrapes").hide();
-				$(".galleryProcessing").hide();
-				$(".galleryReleaseParty").hide();
-			});
-						
-			$(".menuProducts").click(function()
-			{
-				$(".contentHome").hide();
-				$(".contentHistory").hide();
-				$(".contentGallery").hide();
-				$(".contentProducts").show();
-				$(".contentWineClub").hide();
-				$(".contentAbout").hide();
-				$(".contentContactUs").hide();
-				$(".sidePanelHomeLinks" ).show();
-				$(".sidePanelHistoryLinks" ).hide();
-				$(".sidePanelGalleryLinks" ).hide();
-			});
-						
-			$(".menuWineClub").click(function()
-			{
-				$(".contentHome").hide();
-				$(".contentHistory").hide();
-				$(".contentGallery").hide();
-				$(".contentProducts").hide();
-				$(".contentWineClub").show();
-				$(".contentAbout").hide();
-				$(".contentContactUs").hide();
-				$(".sidePanelHomeLinks" ).show();
-				$(".sidePanelHistoryLinks" ).hide();
-				$(".sidePanelGalleryLinks" ).hide();
-			});
-						
-			$(".menuContactUs").click(function()
-			{
-				$(".contentHome").hide();
-				$(".contentHistory").hide();
-				$(".contentGallery").hide();
-				$(".contentProducts").hide();
-				$(".contentWineClub").hide();
-				$(".contentAbout").hide();
-				$(".contentContactUs").show();
-				$(".sidePanelHomeLinks" ).show();
-				$(".sidePanelHistoryLinks" ).hide();
-				$(".sidePanelGalleryLinks" ).hide();
-			});
-
+		//history scripts
 			$(".linkHistoryOfLOW").click(function()
 			{
 				$(".historyOfLOW").show();
@@ -104,15 +15,9 @@ $(document).ready(
 				$(".historyOfLOW").hide();
 				$(".historyOfSDWines").show();
 			});
-						
-			$(".photoGalleryRedGrapes").click(function()
-			{
-				$(".galleryWhiteGrapes").hide();
-				$(".galleryRedGrapes").show();
-				$(".galleryProcessing").hide();
-				$(".galleryReleaseParty").hide();
-			});
-						
+
+		//photo gallery scripts
+
 			$(".photoGalleryWhiteGrapes").click(function()
 			{
 				$(".galleryWhiteGrapes").show();
@@ -300,16 +205,7 @@ $(document).ready(
 				$(".processingImg7Text").hide();
 				$(".processingImg8Text").show();
 			});
-
-			$(".linkPrivacyPolicy").click(function()
-			{
-				$(".menuWineClub").click();
-			});
 			
-			$(".linkTerms").click(function()
-			{
-				$(".menuProducts").click();
-			});
 	});
 
 
